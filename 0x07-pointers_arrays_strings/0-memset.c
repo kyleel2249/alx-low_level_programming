99e95a814249a632ef44da841adf2ebda5ca2871
Kyle El <kyleel249@gmail.com>
2023-03-07 07:40:07 -0800

    101-crackme_password updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * *_memset - fill a block of memory with a specific value
- * @s: starting address of memory area to be filled
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
  *
@@ -11,7 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (;n > 0; i++)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
